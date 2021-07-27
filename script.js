8d30c8a566f0f44bdb6e9d97be20c95229cf4e45
vladislav.vergunov <vladislav.vergunov@icloud.com>
2021-07-27 12:48:18 +0300

    eventListener on yesButton

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const modalTitleYes = document.querySelector('.modal__titleYes'),
-      modalTitle = document.querySelector('.modal__title');
+      modalTitle = document.querySelector('.modal__title'),
+      modalButtonYes = document.querySelector('.modal__button--yes');
+
+modalButtonYes.addEventListener('click', (event) => {
+    modalTitle.classList.toggle('active');
+    modalTitleYes.classList.toggle('active');
+}, {once: true});
+
+
