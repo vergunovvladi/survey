da97b20114f3ce91fbc8b488f4b47516edf37693
vladislav.vergunov <vladislav.vergunov@icloud.com>
2021-07-27 16:52:00 +0300

    top position random

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const modalTitleYes = document.querySelector('.modal__titleYes'),
       modalTitle = document.querySelector('.modal__title'),
-      modalButtonYes = document.querySelector('.modal__button--yes');
+      modalButtonYes = document.querySelector('.modal__button--yes'),
+      modalButtonNo = document.querySelector('.modal__button--no');
 
 modalButtonYes.addEventListener('click', (event) => {
     modalTitle.classList.toggle('active');
     modalTitleYes.classList.toggle('active');
 }, {once: true});
 
+modalButtonNo.addEventListener('mouseenter', (event) => {
+    lol();
+});
 
+let x = 0;
+
+const lol = function () {
+    console.log(modalButtonNo.getBoundingClientRect().top);
+    modalButtonNo.style.top = (Math.random() * (window.innerHeight - modalButtonNo.getBoundingClientRect().height)) - 422.5 + 'px';
+    
+}
